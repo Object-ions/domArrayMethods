044fd8ffc9ab12745efc309d6d2872d752b16b6a
object-ions <moshikoatia@gmail.com>
2023-07-23 16:17:53 -0700

    add users to the UI

--- app.js
+++ app.js
@@ -29,3 +29,22 @@ async function getRandomUser() {
     getRandomUser();
   }
 };
+
+//Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDom();
+}
+
+//Update DOM
+function updateDom(providedData = data) {
+  //Clear the main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    main.appendChild(element);
+  })
+}
